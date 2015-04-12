58f69aaf8a4904b9bbdfaefe8ffddd10a5a9c098
Marco Bachmann <mb@triarc-labs.com>
2015-04-12 20:35:16 +0200

    Bumped version to 0.0.1

---
+++ tlModal.d.ts
@@ -0,0 +1,3 @@
+declare module Triarc.Web.Modal {
+    function openConfirmModal(message: any, $modal: any, title?: string): ng.IPromise<boolean>;
+}
---
+++ tlModal.js
@@ -0,0 +1,44 @@
+var Triarc;
+(function (Triarc) {
+    var Web;
+    (function (Web) {
+        var Modal;
+        (function (Modal) {
+            function openConfirmModal(message, $modal, title) {
+                if (title === void 0) { title = "_defaultNotificationTitle"; }
+                var modalInstance = $modal.open({
+                    templateUrl: 'Client/triarc/Views/partials/notificationModal.html',
+                    size: 'md',
+                    controller: ModalInstanceCtrl,
+                    resolve: {
+                        message: function () {
+                            return message;
+                        },
+                        title: function () {
+                            return title;
+                        }
+                    }
+                });
+                return modalInstance.result.then(function () {
+                    return true;
+                }, function () {
+                    return false;
+                });
+            }
+            Modal.openConfirmModal = openConfirmModal;
+            var ModalInstanceCtrl = function ($scope, $modalInstance, message, title) {
+                $scope.message = message;
+                $scope.title = title;
+            };
+        })(Modal = Web.Modal || (Web.Modal = {}));
+    })(Web = Triarc.Web || (Triarc.Web = {}));
+})(Triarc || (Triarc = {}));
+
+angular.module('tlModal').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('tlModal/notificationModal.html',
+    "<div class=\"modal-header\"><h4 class=\"modal-title\" translate>{{title}}</h4></div><div class=\"modal-body\"><div translate>{{message}}</div></div><div class=\"modal-footer\"><button class=\"btn btn-primary\" ng-click=\"$close(result)\" translate>_ok</button> <button class=\"btn btn-warning\" ng-click=\"$dismiss('cancel')\" translate>_cancel</button></div>"
+  );
+
+}]);
